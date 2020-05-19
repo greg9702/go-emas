27b9a41044c47c4b8d8c57963415d78ba1a28e7f
greg9702 <grzegorzb9702@gmail.com>
2020-05-19 17:19:25 +0200

    Added BaseRandomizer and tests for it

--- pkg/randomizer/randomizer.go
+++ pkg/randomizer/randomizer.go
@@ -1,6 +1,46 @@
 package randomizer
 
+import (
+	"errors"
+	"math/rand"
+	"strconv"
+	"sync"
+	"time"
+)
+
 // IRandomizer interface for radnomizer components
 type IRandomizer interface {
-	RandInt(min int, max int) int
+	RandInt(min int, max int) (int, error)
+}
+
+var singleton *BaseRandomizer
+var once sync.Once
+
+// BaseRand static function returns randomizer
+func BaseRand() *BaseRandomizer {
+	once.Do(func() {
+		singleton = newBaseRandomizer(time.Now().UnixNano())
+	})
+	return singleton
+}
+
+// BaseRandomizer is a base implementaion of IRandomizer
+type BaseRandomizer struct {
+}
+
+func newBaseRandomizer(seed int64) *BaseRandomizer {
+	b := BaseRandomizer{}
+	rand.Seed(seed)
+	return &b
+}
+
+// RandInt returns random value from [min, max] interval, error while passed min > max or negative values
+func (b *BaseRandomizer) RandInt(min int, max int) (int, error) {
+	if min < 0 || min > max {
+		return 0, errors.New("Passed wrong values min: " + strconv.Itoa(min) + " max: " + strconv.Itoa(max))
+	}
+	if min == 0 {
+		return rand.Intn(max + 1), nil
+	}
+	return rand.Intn(max)%(max-min+1) + min, nil
 }
